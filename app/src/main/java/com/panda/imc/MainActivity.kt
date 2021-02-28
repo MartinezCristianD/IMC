70faf590e565d98456c3e4dadea4e0eefd31da91
cristhian martinez <davick16@gmail.com>
2021-02-27 20:19:50 -0500

    functionality 1.0

--- app/src/main/java/com/panda/imc/MainActivity.kt
+++ app/src/main/java/com/panda/imc/MainActivity.kt
@@ -2,74 +2,28 @@ package com.panda.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private val high=findViewById<View>(R.id.high)as Double
-    private val weigth=findViewById<View>(R.id.weigth)as Double
-    private var opp=0.0
-
-
+      private var weigth: Double = 0.0
+      private var high: Double = 0.0
+      private var noOp = " Error"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val result: Button = findViewById(R.id.boton)
         result.setOnClickListener{resultImc()}
     }
         private fun resultImc() {
-            if (this.high in 0.0..250.0 && weigth in 0.0..150.0) {
-                opp = weigth / (this.high * this.high)
-            } else {
-                resultView.text = "error"
-            }
+            val resultView=findViewById<TextView>(R.id.resultView)
+            weigth = findViewById<EditText>(R.id.weigth).toString().toDouble()
+            high= findViewById<EditText>(R.id.high).toString().toDouble()
+
+                if (high in 10.0..250.0 && weigth in 10.0..150.0){
+                resultView.text = (weigth / (high * high)).toString()
+               } else {resultView.text= noOp }
         }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-     /*   showView = "0"
-        val result: Button = findViewById(R.id.boton)
-        result.setOnClickListener(resView())
-    }
-    private var high = findViewById<EditText>(R.id.high)
-    private var weigth = findViewById<EditText>(R.id.weigth)
-    private var showView = findViewById<TextView>(R.id.resultView).toString()
-
-
-    private fun resView(): View.OnClickListener? {
-        val altura = high.toString().toDouble()
-        val peso = weigth.toString().toDouble()
-        val oper = (peso / (altura * altura))
-        showView = when (oper) {
-            in 10.0..70.0 -> oper.toString()
-            else -> "error"
-        }
-    }*/
-
-
-
-
+}
