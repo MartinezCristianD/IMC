4af6bc712232fdae5908eb0f07e6101531b5f536
cristhian martinez <davick16@gmail.com>
2021-03-01 20:11:02 -0500

    Fix property text from edittext an explicit status to heald

--- app/src/main/java/com/panda/imc/MainActivity.kt
+++ app/src/main/java/com/panda/imc/MainActivity.kt
@@ -7,23 +7,49 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-      private var weigth: Double = 0.0
-      private var high: Double = 0.0
-      private var noOp = " Error"
+    private var weigth: Double = 0.0
+    private var high: Double= 0.0
+    private var noOp = " Error"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val result: Button = findViewById(R.id.boton)
-        result.setOnClickListener{resultImc()}
-    }
-        private fun resultImc() {
-            val resultView=findViewById<TextView>(R.id.resultView)
-            weigth = findViewById<EditText>(R.id.weigth).toString().toDouble()
-            high= findViewById<EditText>(R.id.high).toString().toDouble()
 
-                if (high in 10.0..250.0 && weigth in 10.0..150.0){
-                resultView.text = (weigth / (high * high)).toString()
-               } else {resultView.text= noOp }
+
+        val result: Button = findViewById(R.id.boton)
+        result.setOnClickListener { resultImc() }
+    }
+
+    private fun resultImc() {
+        val resultView = findViewById<TextView>(R.id.resultView)
+        weigth = findViewById<EditText>(R.id.weigth).text.toString().toDouble()
+        high = findViewById<EditText>(R.id.high).text.toString().toDouble()
+        high/=100.0
+        high*= high
+
+
+        when (val oper = (weigth / high).toFloat()) {
+            in 0.0..18.4 -> {
+                resultView.text = "Alerta!!Desnutricion su IMC es:$oper"
+            }
+            in 18.5..24.9 -> {
+                resultView.text = "Felicitaciones Normal su IMC es:$oper "
+            }
+            in 25.0..29.9 -> {
+                resultView.text = "Alerta!! Obesidad su IMC es :$oper"
+            }
+            in 30.0..40.0 -> {
+                resultView.text = "Consulte su EPS su IMC es :$oper"
+            }
+            else -> {
+                resultView.text = noOp
+            }
         }
+    }
+    
 }
+
+
+
+
