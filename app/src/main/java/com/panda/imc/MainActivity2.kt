d741eebf3f8282a5d33ac2781909c9c0a43bc8e0
cristhian martinez <davick16@gmail.com>
2021-03-11 14:32:14 -0500

    fix import data, icon.

--- app/src/main/java/com/panda/imc/MainActivity2.kt
+++ app/src/main/java/com/panda/imc/MainActivity2.kt
@@ -8,21 +8,21 @@ import android.widget.EditText
 
 
 class MainActivity2 : AppCompatActivity() {
-    lateinit var name:String
+    lateinit var name:EditText
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        name = findViewById<EditText>(R.id.editTextTextPersonName).text.toString()
+        name = findViewById(R.id.editTextTextPersonName)
 
 
 
         val activity2 = findViewById<Button>(R.id.activity2)
         activity2.setOnClickListener{
             val intent = Intent(this,MainActivity::class.java)
-            intent.putExtra("name",name)
+            intent.putExtra("name",name.text.toString())
             startActivity(intent)
         }
 
