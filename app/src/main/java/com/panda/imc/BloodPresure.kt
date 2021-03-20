b4eab6b5411bee67ee510867b5ab34620fe45fa2
cristhian martinez <davick16@gmail.com>
2021-03-19 22:09:12 -0500

    design implementation blood pressure activity

---
+++ app/src/main/java/com/panda/imc/BloodPresure.kt
@@ -0,0 +1,18 @@
+package com.panda.imc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class BloodPressure : AppCompatActivity() {
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_blood_presure)
+
+    }
+
+    }
